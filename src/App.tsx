43a9028bc7fbecf241cb3c199ad49c4706868bc3
KSY <sykim.p508@gmail.com>
2022-10-04 17:05:22 +0900

    D: can delete selected tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,41 @@
 import React, { useState } from "react";
 import { Task } from "./lib/task";
 
-interface IToDoList {
-  list: Task[];
-}
-
 function App() {
   const [input, setInput] = useState<string>(``);
-
-  const [list, setList] = useState<Task[]>([]);
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
 
-  const onClick = () => {
+  const [list, setList] = useState<Task[]>([]);
+
+  const addItemToList = () => {
     if (input.length > 0) {
       const task = new Task(input);
       setList([...list, task]);
     }
     setInput(``);
   };
-  // const debug = () => console.log(input);
+
+  const clearList = () => setList([]);
+
+  const selectedItem = new Set<string>();
+
+  const onChecked = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.target.checked
+      ? selectedItem.add(e.target.id)
+      : selectedItem.delete(e.target.id);
+    console.log(selectedItem);
+  };
+
+  const deleteSelectedItemsFromList = () => {
+    let newList: Task[] = [...list];
+    selectedItem.forEach((id: string) => {
+      newList = newList.filter((task) => task.id !== id);
+    });
+    setList(newList);
+  };
 
   return (
     <div className="App">
@@ -31,33 +45,26 @@ function App() {
         onChange={onChange}
         placeholder="완벽보다는 완수에 의미를 두자"
       />
-      <button type="button" onClick={onClick}>
+      <button type="button" onClick={addItemToList}>
         submit
       </button>
-      <ToDoList list={list} />
+      <button type="button" onClick={clearList}>
+        clear
+      </button>
+      <ul>
+        {list.map((task: Task) => (
+          <li key={task.id}>
+            <input type="checkbox" id={task.id} onChange={onChecked} />
+            <input type="checkbox" />
+            <label htmlFor={task.id}>{task.title}</label>
+          </li>
+        ))}
+      </ul>
+      <button type="button" onClick={deleteSelectedItemsFromList}>
+        delete
+      </button>
     </div>
   );
 }
 
-function ToDoList({ list }: IToDoList) {
-  return (
-    <ul>
-      {list.map((task: Task) => (
-        <ListItems key={task.id} id={task.id} title={task.title} />
-      ))}
-    </ul>
-  );
-}
-
-function ListItems({ id, title }: Task) {
-  return (
-    <li>
-      <input type="checkbox" id={id} />
-      <label htmlFor={id}>
-        <input type="checkbox" />
-        {title}
-      </label>
-    </li>
-  );
-}
 export default App;
